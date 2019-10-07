f51578fd163eeebc04b8b517fdd4e31c7fc70de9
Gabriel Couture <gacou54@gmail.com>
2019-10-07 14:50:06 -0400

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6'
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ]
 )
